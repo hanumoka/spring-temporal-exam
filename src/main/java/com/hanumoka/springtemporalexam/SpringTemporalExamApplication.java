697b5a69f54cad74ffe765ec4dc4d77d5d92cb32
hanumoka <amagramer@gmail.com>
2026-01-21 10:22:20 +0900

    spring boot 생성

---
+++ src/main/java/com/hanumoka/springtemporalexam/SpringTemporalExamApplication.java
@@ -0,0 +1,13 @@
+package com.hanumoka.springtemporalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemporalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTemporalExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanumoka/springtemporalexam/SpringTemporalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanumoka.springtemporalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemporalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
